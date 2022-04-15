fcfcc6393b89bff531966f3aa57f70b2876c1971
igorandradeio <oigorwebs@gmail.com>
2022-04-15 20:19:05 -0300

    Add admin contact controller

---
+++ app/Http/Controllers/Admin/AdminContactController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreUpdateContact;
+use App\Repositories\ContactRepository;
+use Illuminate\Http\Request;
+
+class AdminContactController extends Controller
+{
+
+    protected $repository;
+
+    public function __construct(ContactRepository $repository)
+    {
+        $this->repository = $repository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $contacts = $this->repository->getContacts();
+
+        return view('admin.contact.index', compact('contacts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.contact.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUpdateContact $request)
+    {
+        $this->repository->createNewContact($request->validated());
+
+        return redirect()->route('admin.contact.home');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $contact = $this->repository->getContactById($id);
+
+        return view('admin.contact.edit', compact('contact'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(StoreUpdateContact $request, $id)
+    {
+        $this->repository->updateContactById($id, $request->validated());
+
+        return redirect()->route('admin.contact.home');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
